cd34fd322fc6a3171bb6e0eb758b724a76a9ae9a
Katerina Voroshilova <kotovaekaterina1@gmail.com>
2024-02-19 14:31:04 +0300

    Task2 Aston

---
+++ src/taskNumbers.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class taskNumbers {
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+        System.out.println("Введите число a: ");
+        int a = in.nextInt();
+
+        System.out.println("Введите число b: ");
+        int b = in.nextInt();
+        
+        if (a<b) {
+            System.out.println("a<b");
+        } else if (a>b) {
+            System.out.println("a>b");
+        }
+        else {
+            System.out.println("a=b");
+        }
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a*b);
+        System.out.println(a/b);
+
+        in.close();
+    }
+}
---
+++ src/taskStrings.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class taskStrings {
+
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+        System.out.println("Введите строку a: ");
+        String a = in.nextLine();
+
+        System.out.println("Введите строку b: ");
+        String b = in.nextLine();
+
+        if (a.equals(b)) {
+            System.out.println("Строки идентичны");
+        }
+        else {
+            System.out.println("Строки не идентичны");
+        }
+
+        in.close();
+    }
+}
